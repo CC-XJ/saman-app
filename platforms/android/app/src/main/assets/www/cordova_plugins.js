bb50f916e32e8c363fd28922713812e2e02c15c4
Ching Xiang Jie <xiangjie.ching@coreconsulting.asia>
2026-01-06 09:45:47 +0800

    Android platform added

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-device": "3.0.0",
+    "cordova-plugin-statusbar": "4.0.0"
+  };
+});
